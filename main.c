426e9ec2dc125c43a94916117b1d41ad62970720
YuichiKamishiro <YuichiKamishiro@gmail.com>
2025-05-07 11:49:54 +0300

    Add basic functions, type asserts

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// strstr
-char *strstr(const char *haystack, const char *needle) {
+enum DebugConfig {
+	EXTRA,
+	FULL,
+	MINIMUM,
+	ONLY_LINE,
+};
+
+static enum DebugConfig debug_config = FULL;
+
+void Init_Debug(enum DebugConfig cfg) {
+	debug_config = cfg;
+}
+
+static char *strstr(const char *haystack, const char *needle) {
     if (!*needle)
         return (char*)haystack;
 
@@ -19,8 +31,15 @@ char *strstr(const char *haystack, const char *needle) {
     return 0;
 }
 
+static int strcmp(const char *s1, const char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return (*(const unsigned char*)s1 - *(const unsigned char*)s2);
+}
 
-#define DEBUG(TEXT) debug(TEXT, __LINE__)
+#define DEBUG(TEXT) debug(TEXT)
 #define DEBUG_ASSERT(TEXT) debug_assert(TEXT, __LINE__)
 #define DEBUG_EQ_STR(TEXT1, TEXT2) debug_eq_str(TEXT1, TEXT2, __LINE__)
 #define DEBUG_EQ_INT(TEXT1, TEXT2) debug_eq_int(TEXT1, TEXT2, __LINE__)
@@ -32,73 +51,139 @@ char *strstr(const char *haystack, const char *needle) {
 
 // implementation to exit
 int stop_execution() {
-    //exit(69);
 }
 
 // implementation to write message
-static inline void debug(const char message[], int line) {
-	printf("%s in line %d\n", message, line);
+static inline void debug(const char message[]) {
+	printf("%s\n", message);
 }
 
-static inline void debug_assert(unsigned int a, int line) {
-	if(!a) {
-		const char message[] = "ASSERTION FAILED";
-		debug(message, line);
-		stop_execution();
-	}	
+enum TypeError {
+	ASSERT,
+	EQ_INT,
+	EQ_STR,
+	ENDS,
+	ENDS_CRLF,
+	CONTAIN,
+};
+
+static inline void format(enum TypeError err_type,
+						  int line,
+						  const char *details)
+{
+	char label[128];
+	char output[256];
+
+	/* Формируем базовую метку без номера строки и деталей */
+	switch (err_type)
+	{
+	case ASSERT:
+		snprintf(label, sizeof(label), "ASSERTION FAILED");
+		break;
+	case EQ_INT:
+		snprintf(label, sizeof(label), "INT ASSERTION FAILED");
+		break;
+	case EQ_STR:
+		snprintf(label, sizeof(label), "STRING ASSERTION FAILED");
+		break;
+	case ENDS_CRLF:
+		snprintf(label, sizeof(label), "CRLF ASSERTION FAILED");
+		break;
+	case CONTAIN:
+		snprintf(label, sizeof(label), "CONTAINS ASSERTION FAILED");
+		break;
+	default:
+		snprintf(label, sizeof(label), "UNKNOWN ERROR");
+	}
+
+	switch (debug_config)
+	{
+	case EXTRA:
+		/* label + line + details */
+		snprintf(output, sizeof(output),
+				 "%s at line %d: %s",
+				 label, line, details);
+		break;
+	case FULL:
+		/* label + line */
+		snprintf(output, sizeof(output),
+				 "%s at line %d",
+				 label, line);
+		break;
+	case MINIMUM:
+		/* только label */
+		snprintf(output, sizeof(output),
+				 "%s",
+				 label);
+		break;
+	case ONLY_LINE:
+		/* только номер строки */
+		snprintf(output, sizeof(output),
+				 "Line %d",
+				 line);
+		break;
+	}
+
+	debug(output);
+	stop_execution();
+}
+static inline void debug_assert(unsigned int expr, int line) {
+    if (!expr) {
+        format(ASSERT, line, "");
+    }
 }
 
 static inline void debug_eq_int(int a, int b, int line) {
-	if(a != b) {
-		const char info[] = "INT ASSERTION FAILED";
-		char message[64];
-		sprintf(message, "%s %d != %d", info, a, b);
-		debug(message, line);
-		stop_execution();
-	}
+    if (a != b) {
+        char details[64];
+        snprintf(details, sizeof(details), "%d != %d", a, b);
+        format(EQ_INT, line, details);
+    }
 }
 
 static inline void debug_eq_str(const char a[], const char b[], int line) {
-	if(a != b) {
-		const char info[] = "STRING ASSERTION FAILED";
-		char message[64];
-		sprintf(message, "%s %s != %s", info, a, b);
-		debug(message, line);
-		stop_execution();
-	}
+    if (strcmp(a, b) != 0) {
+        char details[128];
+        snprintf(details, sizeof(details), "\"%s\" != \"%s\"", a, b);
+        format(EQ_STR, line, details);
+    }
 }
 
-static inline void debug_array_ends_crlf(const char a[], int len, int line) {
-	if(! (a[len - 1] == '\n' && a[len - 2] == '\r') ) {
-		const char info[] = "ARRAY ASSERTION FAILED";
-		char message[64];
-		sprintf(message, "%s %s NOT end with CRLF", info, a);
-		debug(message, line);
-		stop_execution();
-	}
+static inline void debug_array_ends_crlf(const char a[],
+                                         int len,
+                                         int line)
+{
+    if (!(a[len-2] == '\r' && a[len-1] == '\n')) {
+        char details[128];
+        snprintf(details, sizeof(details),
+                 "expected CRLF at end of \"%s\"", a);
+        format(ENDS_CRLF, line, details);
+    }
 }
 
-static inline void debug_array_contains(const char a[], const char b[], int line) {
-	if(!(strstr(a, b))) {
-		const char info[] = "ARRAY ASSERTION FAILED";
-		char message[64];
-		sprintf(message, "%s %s NOT contains %s", info, a, b);
-		debug(message, line);
-		stop_execution();
-	}    
+static inline void debug_array_contains(const char a[],
+                                        const char b[],
+                                        int line)
+{
+    if (!strstr(a, b)) {
+        char details[128];
+        snprintf(details, sizeof(details),
+                 "\"%s\" not found in \"%s\"", b, a);
+        format(CONTAIN, line, details);
+    }
 }
 
 int main()
 {
-
-    DEBUG("Debug Message");
+	Init_Debug(ONLY_LINE);
+    DEBUG("Debug Message\n");
     
-    DEBUG_ASSERT(1 == 1);
+    DEBUG_ASSERT(2 == 1);
     
-    const char msg[] = "hellol";
+    const char msg[] = "hello";
     DEBUG_ARRAY_ENDS_CRLF(msg);
     
-    const char word[] = "lo";
+    const char word[] = "hui";
     
     DEBUG_ARRAY_CONTAINS(msg, word);
     
